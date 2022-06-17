ccaf72f2a5a1387ee5c072973346ef2f64e12718
Bo Bunmeng <bunmeng.bo@gmail.com>
2022-06-17 18:34:40 +0200

    first commit

---
+++ cardscoring/cardscoringApp.swift
@@ -0,0 +1,17 @@
+//
+//  cardscoringApp.swift
+//  cardscoring
+//
+//  Created by Bunmeng Bo on 17.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct cardscoringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
